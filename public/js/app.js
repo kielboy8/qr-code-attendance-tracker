02ddd1e3a0b0a7a0f43e785951646f21e4e35aad
kielboy8 <kielboy8@gmail.com>
2018-07-19 13:50:30 +0800

    minor fix in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -13915,16 +13915,6 @@ var app = new Vue({
         $(".menu-toggle").removeClass("active");
         $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     });
-
-    // Scroll to top button appear
-    $(document).scroll(function () {
-        var scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
-    });
 })(jQuery); // End of use strict
 
 /***/ }),
